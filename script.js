acced0a1046741f29c93537c566e6dbab83cc9bf
Ansh1221580288 <anshshukla7227@gmail.com>
2026-07-23 19:16:26 +0530

    Basic Code 1

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function sum (){
     const a = 50;
     const b = 41;
-    const c = a + b ;
-    console.log(c);   // for printing  in console  window 
-    document.write(c);  // for printing in browser 
+    // const c = a + b ;
+    return a + b ;
+    // console.log(c);   // for printing  in console  window 
+    // document.write(c);  // for printing in browser 
 }
 
 // sum();
