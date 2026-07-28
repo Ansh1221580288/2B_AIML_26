a0599f8e01ecad34fc6b5eb2f8fd0497111f981d
Ansh1221580288 <anshshukla7227@gmail.com>
2026-07-28 11:54:37 +0530

    Basic Code 2

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function sum (){
     const a = 50;
     const b = 41;
-    // const c = a + b ;
-    return a + b ;
+    const c = a + b ;
+    // return a + b ;
     // console.log(c);   // for printing  in console  window 
-    // document.write(c);  // for printing in browser 
+    document.write(c);  // for printing in browser 
 }
 
 // sum();
 // console.log(sum); // for printing the sum function
+
+
+//------------------------------------------------------------ pop up box -----------------------------------------------------------------------------
+
+//   alert()
+//   prompt()  //to take input from user
+//   confirm()
+
+
+function vote(){
+    let age = parseInt(prompt("Enter your age : "))
+    if(age>=18){
+        alert("You are eligible for Voting");
+    }else{
+        alert("You are not eligible");
+    }
+}
