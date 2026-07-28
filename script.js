2a65d2d7ddb930be850f3953871f97f6988de028
Ansh1221580288 <anshshukla7227@gmail.com>
2026-07-28 11:59:01 +0530

    Basic Code 3

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// function sum (){
-//     const a = 50;
-//     const b = 41;
-//     const c = a + b ;
+function sum (){
+    const a = 50;
+    const b = 41;
+    const c = a + b ;
     // return a + b ;
     // console.log(c);   // for printing  in console  window 
-    // document.write(c);  // for printing in browser 
-// }
+    document.write(c);  // for printing in browser 
+}
 
 // sum();
 // console.log(sum); // for printing the sum function
