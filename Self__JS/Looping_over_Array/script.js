de357c28877cc8e81b50d1e33b76bb2a5e7a36d1
Ansh1221580288 <anshshukla7227@gmail.com>
2026-09-02 20:11:10 +0530

    Looping_over_Array

---
+++ Self__JS/Looping_over_Array/script.js
@@ -0,0 +1,22 @@
+let marks = [100,98,98,44,55,88];
+
+//For loop
+
+for(let i=0; i<marks.length; i++){
+    console.log(marks[i]);
+}
+
+
+//for of loop
+
+// for(let val of marks){
+//     console.log(val);
+// }
+
+//for  in loop
+
+// for(let idx in marks){
+//     console.log(idx);
+// }
+
+
