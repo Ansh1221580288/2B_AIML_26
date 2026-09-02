c269942d6d479545bf633a52d0cedf3d9eb1b933
Ansh1221580288 <anshshukla7227@gmail.com>
2026-09-02 22:01:27 +0530

    College_JS

---
+++ College_JS/other/script.js
@@ -0,0 +1,52 @@
+// function sum (){
+//     const a = 50;
+//     const b = 41;
+//     const c = a + b ;
+//     // return a + b ;
+//     // console.log(c);   // for printing  in console  window 
+//     document.write(c);  // for printing in browser 
+// }
+
+// sum();
+// console.log(sum); // for printing the sum function
+
+
+//------------------------------------------------------------ pop up box -----------------------------------------------------------------------------
+
+//   alert()
+//   prompt()  //to take input from user
+//   confirm()
+
+
+// function vote(){
+//     let age = (prompt("Enter your age : "))
+//     if(age>=18){
+//         alert("You are eligible for Voting");
+//     }else{
+//         alert("You are not eligible");
+//     }
+// }
+
+
+// function vote(){
+//     let age = (prompt("Enter your age : "))
+//     if(age>=18){
+//        let c = confirm("Do you want to submit your response");
+//        {
+//         if(c){
+//             alert("Your response is submitted");
+//         }else{
+//             alert("Thank you !")
+//         }
+//        }
+//     }else{
+//         alert("You are not eligible");
+//     }
+// }
+// Object in JavaScript
+
+let A = newArray();
+A[0] = "Hello";
+A[1] = "World";
+A[2] = "!";  
+console.log(A[0] + " " + A[1] + " " + A[2]);   
