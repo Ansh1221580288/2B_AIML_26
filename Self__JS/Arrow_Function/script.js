9d6ed82010dc88d9816c052004dc60ed292f52f6
Ansh1221580288 <anshshukla7227@gmail.com>
2026-09-03 12:03:33 +0530

    Minor change

--- Self__JS/Arrow_Function/script.js
+++ Self__JS/Arrow_Function/script.js
@@ -4,14 +4,16 @@
 // Syntax of an arrow function
 // The basic syntax of an arrow function is as follows:
 
-() => {
-  // function body
-}
+// () => {
+//   // function body
+// }
 // Now we can store it in new variable and call it like a normal function
-const myFunction = () => {
-  console.log("Hello, World!");
-}
-myFunction(); // Output: Hello, World!
+
+//curly braces are optional if the function body contains only a single statement. In that case, you can omit the curly braces and the return keyword, and the value of the expression will be returned automatically.
+// const myFunction = () => {
+//   console.log("Hello, World!");
+// }
+// myFunction(); // Output: Hello, World!
 
 // we can also pass parameters to an arrow function.
 
@@ -19,3 +21,4 @@ const add = (a, b) => {
   return a + b;
 }
 add(5, 3); // Output: 8
+console.log(add(5, 3)); // Output: 8 
